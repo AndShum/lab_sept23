39d8191074c2b2633be3af67e84e96cfc156798b
AndShum <ashumway2@gmail.com>
2016-09-23 15:32:52 -0700

    question 3- 5 done wrong

--- app.js
+++ app.js
@@ -106,11 +106,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+// var multipleArray = [2,3,4];
+var productOfMultipleArray = 2 * 3 * 4;
+var messageForMultipleArray = 'The numbers 2,3,4 have a product of ' + productOfMultipleArray + '.';
+var outputForMultipleArray = [productOfMultipleArray, messageForMultipleArray];
 function multiplyArray(testArray){ //eslint-disable-line
-
+  return outputForMultipleArray;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
